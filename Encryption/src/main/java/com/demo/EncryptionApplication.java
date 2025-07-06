edff421345f9cb0fb4d8ccb9ddeaf998e392ca01
Srikanth Machunuri <sriyadav326@gmail.com>
2025-07-06 20:38:18 +0530

    new commit

--- Encryption/src/main/java/com/demo/EncryptionApplication.java
+++ Encryption/src/main/java/com/demo/EncryptionApplication.java
@@ -2,15 +2,20 @@ package com.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.demo"})
-public class EncryptionApplication {
+public class EncryptionApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EncryptionApplication.class, args);
 		System.out.println("==============Build Success of Encryption Project==============");
 	}
-
+	@Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	        return builder.sources(EncryptionApplication.class);
+	    }
 }
