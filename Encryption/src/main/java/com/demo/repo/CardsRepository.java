8c32f4c6b9d781172fe8a3b7adced9b88d42306c
Srikanth Machunuri <sriyadav326@gmail.com>
2025-07-09 23:54:38 +0530

    jpa classes changes.

--- Encryption/src/main/java/com/demo/repo/CardsRepository.java
+++ Encryption/src/main/java/com/demo/repo/CardsRepository.java
@@ -4,14 +4,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.demo.ml.DebitCard;
 
+@Repository
 public interface CardsRepository extends JpaRepository<DebitCard, Long> {
 
     @Modifying
     @Transactional
-    @Query("UPDATE EncryptionBean d SET d.filePath = :filePath WHERE d.accountId = :accountId")
+    @Query("UPDATE DebitCard d SET d.filePath = :filePath WHERE d.accountId = :accountId")
     int updateFilePath(@Param("filePath") String filePath, @Param("accountId") Long accountId);
 }
